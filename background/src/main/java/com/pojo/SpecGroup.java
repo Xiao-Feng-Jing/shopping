593794a4202a0c7fe282d0ffdf72b21e8564b309
fear101 <zk1747793@163.com>
2020-11-13 16:38:29 +0800

    更新0.4

---
+++ background/src/main/java/com/pojo/SpecGroup.java
@@ -0,0 +1,47 @@
+package com.pojo;
+
+
+public class SpecGroup {
+
+  private long id;
+  private long cid;
+  private String name;
+  private long status;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public long getCid() {
+    return cid;
+  }
+
+  public void setCid(long cid) {
+    this.cid = cid;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public long getStatus() {
+    return status;
+  }
+
+  public void setStatus(long status) {
+    this.status = status;
+  }
+
+}
---
+++ background/src/main/java/com/pojo/SpecParam.java
@@ -0,0 +1,67 @@
+package com.pojo;
+
+
+public class SpecParam {
+
+  private long id;
+  private long cid;
+  private long groupId;
+  private String name;
+  private long generic;
+  private long status;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public long getCid() {
+    return cid;
+  }
+
+  public void setCid(long cid) {
+    this.cid = cid;
+  }
+
+
+  public long getGroupId() {
+    return groupId;
+  }
+
+  public void setGroupId(long groupId) {
+    this.groupId = groupId;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public long getGeneric() {
+    return generic;
+  }
+
+  public void setGeneric(long generic) {
+    this.generic = generic;
+  }
+
+
+  public long getStatus() {
+    return status;
+  }
+
+  public void setStatus(long status) {
+    this.status = status;
+  }
+
+}
